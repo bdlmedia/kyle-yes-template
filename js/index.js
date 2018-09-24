f1227e3328acdf539d70c62bcf4f0cc5bb75ec97
Kyle Wilson <mike@performancemotif.com>
2018-09-24 16:39:18 -0700

    moved js/css to separate files

---
+++ js/index.js
@@ -0,0 +1,42 @@
+$(document).ready(function() {
+
+				$("#next-1").click(function() {
+					$("#second").show();
+					$("#first").hide();
+					$("#progressBar").css("width", "60%");
+					$("#progressText").text("Step - 2");
+
+				});
+
+				$("#next-2").click(function() {
+					$("#third").show();
+					$("#second").hide();
+					$("#progressBar").css("width", "100%");
+					$("#progressText").text("Step - 3");
+
+				});
+
+				$("#prev-2").click(function() {
+					$("#second").hide();
+					$("#first").show();
+					$("#progressBar").css("width", "20%");
+					$("#progressText").text("Step - 1");
+
+				});
+
+				$("#prev-3").click(function() {
+					$("#third").hide();
+					$("#second").show();
+					$("#progressBar").css("width", "60%");
+					$("#progressText").text("Step - 2");
+				});
+
+				$("#submit").click(function() {
+					$(".container").hide();
+					$(".searchingBox").show();
+
+
+				});
+
+
+			});
